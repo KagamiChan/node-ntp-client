f5e8d65425154b8eabdacf729552c5b7b7b99570
Kagami <moe.pascal@gmail.com>
2017-04-29 02:15:44 +0800

    es6+

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  'extends': 'airbnb-base',
+  'plugins': [
+    'import',
+  ],
+  'rules': {
+    'comma-dangle': ['error', 'always-multiline'],
+    'semi': ['error', 'never'],
+  }
+}
